dc177bbc3126f08074e44db4423d4fa96cd52286
Areeba Siddique <areebasiddique021@gmail.com>
2024-06-11 23:23:52 -0700

    Add files via upload

---
+++ express/index.js
@@ -0,0 +1,10 @@
+const express = require ('express');
+const app = express();
+const port= 3000;
+app.get('/', (req, res)=>{
+    res.send('hello world !');
+});
+
+app.listen(port,()=>{
+    console.log(`Server is running on http localhost:${port} `);
+});
